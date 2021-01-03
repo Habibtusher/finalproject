8f6ca09e570830ab7af9446eef03e33f40bb4ac4
Habibtusher <tusherhrt@gmail.com>
2021-01-03 14:44:08 +0600

    first commit

---
+++ question.js
@@ -0,0 +1,82 @@
+var question = [{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of India?",
+  "option1":"Chennai",
+  "option2":"Delli",
+  "option3":"Mumbai",
+  "option4":"Rajasthan",
+  "answer":2
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+},
+{
+  "question":"What is the capital city of Bangladesh?",
+  "option1":"Dhaka",
+  "option2":"Chottogram",
+  "option3":"Noakhali",
+  "option4":"Rangpur",
+  "answer":1
+}
+
+]
